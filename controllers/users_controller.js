628098acdfe23f23062828a6583398de542dd183
Nikhil Goduguchintha <nikhilnikki747@gmail.com>
2022-11-17 22:15:09 +0530

    completed till passport Js

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,5 @@
+const User = require("../models/user");
+
 module.exports.profile = function(req, res){
     return res.render('user_profile', {
         title: 'User Profile'
@@ -5,13 +7,51 @@ module.exports.profile = function(req, res){
 }
 
 module.exports.signin = function(req,res){
+    if(req.isAuthenticated()){
+        return res.redirect('/users/profile');
+    }
     res.render('user_sign_in',{
         title: 'Sign In'
     })
 }
 
 module.exports.signup = function(req,res){
+    if(req.isAuthenticated()){
+        return res.redirect('/users/profile');
+    }
     res.render('user_sign_up',{
         title: 'Sign Up'
     })
+}
+
+
+// get the signup data
+module.exports.create = function(req,res){
+    // the below code is manual authentication
+    if(req.body.password!= req.body.confirm_password){
+        return res.redirect('back')
+    }
+    User.findOne({email: req.body.email},function(err,user){
+        if(err){console.log('error in finding user in sining up');return}
+
+        if(!user){
+            User.create(req.body, function(err,user){
+                if(err){console.log('error is creating user while signing up'); return}
+                return res.redirect('/users/sign-in')
+            })
+        }else{
+            return res.redirect('back');
+        }
+    })
+}
+
+module.exports.createSession = function(req,res){
+    return res.redirect('/');
+}
+
+module.exports.destroySession = function(req,res){
+    req.logout(function(){
+        
+    });
+    return res.redirect('/');
 }
--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
 const app = express();
 const port = 8000;
 const expressLayouts = require('express-ejs-layouts');
 const db = require('./config/mongoose');
+const session = require('express-session');
+const passport = require('passport')
+const passportLocal = require('./config/passport-local-strategy');
+const MongoStore = require('connect-mongo');
+
+app.use(express.urlencoded());
+
+app.use(cookieParser());
 
 app.use(express.static('./assets'));
 
@@ -12,13 +21,38 @@ app.set('layout extractStyles', true);
 app.set('layout extractScripts', true);
 
 
-// use express router
-app.use('/', require('./routes'));
-
 // set up the view engine
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
+// mongo store(mongo connect) is used to store the session cookie in the db
+app.use(session({
+    name: 'Codeial',
+    // todo change the secret before deployement in prod
+    secret: 'blahSomething',
+    saveUninitialized: false,
+    resave:false,
+    cookie:{
+        maxAge:(1000 * 60 *100)
+    },
+    store: MongoStore.create({
+        mongoUrl: 'mongodb://localhost/codeial_development_main',
+        autoRemove: 'disable',
+        touchAfter: 1 * 3600
+    },
+    function(err){
+        console.log(err || 'Connect -mongodb setup ok' )
+    }
+    )
+}));
+// say app to use passport
+app.use(passport.initialize());
+// say app to use session
+app.use(passport.session());
+
+app.use(passport.setAuthenticatedUser);
+// use express router
+app.use('/', require('./routes'));
 
 app.listen(port, function(err){
     if (err){
